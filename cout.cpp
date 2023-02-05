f860d2cdad0f4fb5ff70720d0ce4180d1e4c8c26
Claudiu Gombos <claudiu.gombos@thinslices.com>
2023-02-05 19:48:25 +0200

    added cout

---
+++ cout.cpp
@@ -0,0 +1,14 @@
+// C++ program to illustrate the use
+// of cout object
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    // Print standard output
+    // on the screen
+    cout << "Welcome to GFG";
+ 
+    return 0;
+}
